d383b4a74966190ed52c0af00b0b8e21a0ede96e
qiukai <943451277@qq.com>
2022-02-25 19:35:17 +0800

    qk

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <m-header></m-header>
+</template>
+
+<script>
+import Header from '@/components/header/header'
+
+export default {
+  components: {
+    MHeader: Header
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/components/tab/tab.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="tab">
+    <router-link class="tab-item" v-for="tab in tabs" :key="tab.path" :to="tab.path">
+      <span class="tab-link">
+        {{tab.name}}
+      </span>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tabs: [
+        {
+          name: '推荐',
+          path: '/recommend'
+        },
+        {
+          name: '歌手',
+          path: '/singer'
+        },
+        {
+          name: '排行',
+          path: '/top-list'
+        },
+        {
+          name: '搜索',
+          path: '/search'
+        }
+      ]
+    }
+  }
+}
+</script>
+<style  lang='scss' scoped>
+</style>
